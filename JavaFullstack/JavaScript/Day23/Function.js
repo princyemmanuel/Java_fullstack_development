b85151ff3bae317f2d64b7654be891acaae1e18d
Princy Emmanuel <73642019+princyemmanuel@users.noreply.github.com>
2024-04-05 23:09:44 -0500

    Add files via upload

---
+++ JavaFullstack/JavaScript/Day23/Function.js
@@ -0,0 +1,15 @@
+function add(){
+    var num1 = 100;
+    var num2 =200;
+
+    var result = num1+num2;
+    console.log("Addition = ",result)
+}
+function sub(){
+    var value1=300;
+    var value2=150;
+    var result = value1-value2;
+    console.log("Substraction = ",result)
+}
+add();
+sub();
