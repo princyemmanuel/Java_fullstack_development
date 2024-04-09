9a2a2d7dac5df760f2f4fdb9dd07923982c34532
Princy Emmanuel <73642019+princyemmanuel@users.noreply.github.com>
2024-04-08 23:39:37 -0500

    Add files via upload

---
+++ JavaFullstack/JavaScript/Day24/Operator.js
@@ -0,0 +1,25 @@
+var A =800;
+var B = 2;
+
+var Result3 = A+B; //A+B
+var Result4 = A-B;//A-B
+var Result5 = A*B;//A*B
+var Result6 = A/B;//A/B ==> RETURN QUOTIENT
+var Result7 = A%B;//A%B ==> RETURN REMINDER
+var Result8 = A**B;// (A)B
+var Result9 = A++;//a= a+1   => Post Increment
+var Result10 = ++A;//a=a+1   => pre increment
+var Result11 = A--;//a= a-1  => post Decrement
+var Result12 = --A;//a= a-1  =>pre Decrement
+
+console.log(Result3);
+console.log(Result4);
+console.log(Result5);
+console.log(Result6);
+console.log(Result7);
+console.log(Result8);
+console.log(Result9);
+console.log(Result10);
+console.log(Result11);
+console.log(Result12);
+
---
+++ JavaFullstack/JavaScript/Day24/Relational.js
@@ -0,0 +1,24 @@
+var A =80;
+var B = 40;
+
+var Result1 = A>B; // true
+
+var Result2 = A<B; //false
+
+var Result3 = A>=B; //true
+var Result4 = A<=B; //false
+var Result5 = A==B; //false 
+                       //"80"==80 ==>true
+                       
+var Result6 = A===B; //false
+                    //"80"==80 ==>false ==>>>  chech for data type
+                    
+var Result7 = A!=B; //true
+console.log(Result1);
+console.log(Result2);
+console.log(Result3);
+console.log(Result4);
+console.log(Result5);
+console.log(Result6);
+console.log(Result7);
+
